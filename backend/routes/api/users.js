4f4901738ae61789881620920f591cd942bd45da
Ben <ben.jr.speed@gmail.com>
2022-08-05 01:35:56 -0400

    signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
 const router = express.Router();
+
+router.post('/', asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signUp({ email, password, username });
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+}));
 
 module.exports = router;
